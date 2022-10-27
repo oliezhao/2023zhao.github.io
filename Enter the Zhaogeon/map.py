869ae9e3f36431783d84490b5a1658f61ac9244d
Money in the bank <oliezhao@outlook.com>
2022-10-27 21:41:56 +0100

    idk wtf

--- Enter the Zhaogeon/map.py
+++ Enter the Zhaogeon/map.py
@@ -42,8 +42,6 @@ class Map():
                     self.player = Player([collum_i * size, row_i *size], self.player_speed, self.player_spawn_sprite)
                     self.player_spritegroup.add(self.player)
 
-<<<<<<< HEAD
-=======
     def collision_detect(self, direction):
 
         walls_in_contact = sprite.spritecollide(self.player, self.wall_spritegroup, False) #Creates a list of all walls in collision with player
@@ -71,11 +69,26 @@ class Map():
 
     def camera_detect(self, direction):
 
+        # if not(sprite.collide_rect(self.player, self.camera)): #if player is out of bounds with camera
+
+        #     if self.player.rect.right < self.camera.rect.left: #detects if the player is to the left or right of the camera hitbox
+        #         self.player.rect.right = self.camera.rect.left + 1#teleports player back to the according side of the camera hitbox
+        #     elif self.player.rect.left > self.camera.rect.right: 
+        #         self.player.rect.left = self.camera.rect.right - 1
+                
+        #     if self.player.rect.top > self.camera.rect.bottom: #detects if the player is to the top or bottom of the camera
+        #         self.player.rect.top = self.camera.rect.bottom - 1#teleports the player to the according side of the camera hitbox
+        #     elif self.player.rect.bottom < self.camera.rect.top: 
+        #         self.player.rect.bottom = self.camera.rect.top + 1
+                
+        #     for self.wall in self.wall_spritegroup: #change to "for element in self.nonplayer_spritegroup" at later date
+        #         self.wall.rect.x -= self.player.speed.x #because player is being teleport back to camera hitbox, all assets move in oppsite direcction of player, with player speed to simulate player movement
+        #         self.wall.rect.y -= self.player.speed.y
+        
         if not(sprite.collide_rect(self.player, self.camera)):
             for wall in self.wall_spritegroup:
                 if direction == "x":
                     if self.player.rect.right < self.camera.rect.left: #detects if the player is to the left or right of the camera hitbox
-                        
                         self.player.rect.right = self.camera.rect.left + 1#teleports player back to the according side of the camera hitbox
                     elif self.player.rect.left > self.camera.rect.right: 
                         self.player.rect.left = self.camera.rect.right - 1
@@ -89,43 +102,15 @@ class Map():
                         self.player.rect.bottom = self.camera.rect.top + 1
                     
                     wall.rect.y -= self.player.speed.y
->>>>>>> parent of 4faa2bf (Debug Fix)
     def move(self):
-        
-<<<<<<< HEAD
-        self.player.rect.x += self.player.speed.x
-        self.player.rect.y += self.player.speed.y
-        
-        if sprite.spritecollide(self.player):
-        
-        if not(sprite.collide_rect(self.player, self.camra)):
-=======
-        #x axis
+
         self.player.rect.x += self.player.speed.x #moves players
         if not(self.collision_detect("x")): self.camera_detect("x")
+
         
-        #y axis
         self.player.rect.y += self.player.speed.y
         if not(self.collision_detect("y")): self.camera_detect("y")
->>>>>>> parent of 4faa2bf (Debug Fix)
 
-            if self.player.rect.right < self.camra.rect.left: 
-                self.player.rect.right = self.camra.rect.left + 1
-            elif self.player.rect.left > self.camra.rect.right: 
-                self.player.rect.left = self.camra.rect.right - 1
-                
-            if self.player.rect.top > self.camra.rect.bottom: 
-                self.player.rect.top = self.camra.rect.bottom - 1
-            elif self.player.rect.bottom < self.camra.rect.top: 
-                self.player.rect.bottom = self.camra.rect.top + 1
-                
-            for self.wall in self.wall_spritegroup:
-                self.wall.rect.x -= self.player.speed.x
-                self.wall.rect.y -= self.player.speed.y
-        else:
-            self.worldshift.x = 0
-            self.worldshift.y = 0
-    
     def update(self, cursor_pos):
         #passes cursor position to player
         print(self.player.speed)
