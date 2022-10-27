4faa2bf720b9813c03c0be0fecc12f37212d674f
Money in the bank <oliezhao@outlook.com>
2022-10-27 21:09:59 +0100

    Debug Fix
    Realisation- previous solution would not work. Resetting.

--- Enter the Zhaogeon/map.py
+++ Enter the Zhaogeon/map.py
@@ -52,21 +52,21 @@ class Map():
             for wall in walls_in_contact: #checks for very wall in contact
                 if direction == "x": #if checking for the x axis
                     if self.player.move_direction.x > 0: #if player is moving right
+                        return "pw" #player is moving right into the wall
                         self.player.rect.right = wall.rect.left #the player's right is teleported to the walls left
-                        return True
                     elif self.player.move_direction.x < 0: #if the player is moving left
+                        return "wp" #player is moving left int othe wall
                         self.player.rect.left = wall.rect.right #the player's left is teleported to the walls right
-                        return  True
                     else:
                         return False
 
                 if direction == "y": #if checking for y axis
                     if self.player.move_direction.y > 0: #if player is moving down
+                        return "p/w" #player is moving down into the wall
                         self.player.rect.bottom = wall.rect.top #players bottom is teleported to the walls top
-                        return True
                     elif self.player.move_direction.y < 0: #if player is moving up
+                        return "w/p" #player is moving up into the wall
                         self.player.rect.top = wall.rect.bottom #players top is teleported to walls bottom
-                        return True
                     else:
                         return False
 
@@ -76,17 +76,20 @@ class Map():
             for wall in self.wall_spritegroup:
                 if direction == "x":
                     if self.player.rect.right < self.camera.rect.left: #detects if the player is to the left or right of the camera hitbox
-                        
+                        return "pc" #player is the left of camera
                         self.player.rect.right = self.camera.rect.left + 1#teleports player back to the according side of the camera hitbox
                     elif self.player.rect.left > self.camera.rect.right: 
+                        return "cp" #player is right of camera
                         self.player.rect.left = self.camera.rect.right - 1
 
                     wall.rect.x -= self.player.speed.x
 
                 if direction == "y":
                     if self.player.rect.top > self.camera.rect.bottom: #detects if the player is to the top or bottom of the camera
+                        return "c/p" #player is below wall
                         self.player.rect.top = self.camera.rect.bottom - 1#teleports the player to the according side of the camera hitbox
                     elif self.player.rect.bottom < self.camera.rect.top: 
+                        return "p/c" #player is above wall
                         self.player.rect.bottom = self.camera.rect.top + 1
                     
                     wall.rect.y -= self.player.speed.y
@@ -101,10 +104,17 @@ class Map():
         
         #x axis
         self.player.rect.x += self.player.speed.x #moves players
-        if not(self.collision_detect("x")): self.camera_detect("x")
+        
+        if self.collision_detect("x"):
+        if self.camera_detect("y"): 
+        
+        #if not(self.collision_detect("x")): self.camera_detect("x")
         
         #y axis
         self.player.rect.y += self.player.speed.y
+        
+        if self.collision_detect("y"):
+        if self.camera_detect("y"):
         if not(self.collision_detect("y")): self.camera_detect("y")
 
         #self.player.speed is calculated in Player Class, ran in update through player.update (which rans player.speed_calcu)
